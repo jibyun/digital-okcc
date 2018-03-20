7ec3111526f00c1ffd0b791180f31869a9c4a304
kiwoong <kw2446@gmail.com>
2018-03-19 21:27:17 -0400

    Controller 추가

---
+++ laravel/app/Http/Controllers/TestController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+   
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('test');
+    }
+
+
+	public function test2()
+    {
+        return view('test2');
+    }
+
+
+}
