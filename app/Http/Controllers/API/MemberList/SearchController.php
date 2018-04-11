4ec9a660ec89f38b11f313ec72ce19397239f2c4
Gyoun Lee <gyounlee@yahoo.ca>
2018-04-11 17:18:42 -0400

    Search member by name

---
+++ app/Http/Controllers/API/MemberList/SearchController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\API\MemberList;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\API\BaseController;
+use App\Http\Services\MemberList\SearchService;
+
+class SearchController extends BaseController
+{
+    private $searchService;
+
+    public function __construct() {
+        $this->searchService = new SearchService();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  string  $search
+     * @return \Illuminate\Http\Response
+     */
+    public function show($search)
+    {
+        return $this->sendResponse(json_encode($this->searchService->getMemberList($search)),
+                                    "retrieved member successfully.");
+    }
+
+}
--- resources/views/MemberList/mainPanel.blade.php
+++ resources/views/MemberList/mainPanel.blade.php
@@ -1,4 +1,7 @@
 <div id="divMainPanel">
 {{--TODO: We will add contents here--}}
+{{-- page title --}}
+<div id="pageTitle"></div>
+{{-- table --}}
 <div id="mainTable"></div>
 </div>
