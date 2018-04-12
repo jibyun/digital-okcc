021d626b0bc25764061977c50e6b7eaef0f6efab
Byung Seon Kim <lifeisox@gmail.com>
2018-04-12 18:25:52 -0400

    Register User including Send Admin email

--- config/services.php
+++ config/services.php
@@ -15,8 +15,8 @@ return [
     */
 
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
-        'secret' => env('MAILGUN_SECRET'),
+        'domain' => env('MAILGUN_DOMAIN', 'okcc.ca'),
+        'secret' => env('MAILGUN_SECRET', 'key-47840875b9aa764cd4ed23197bf2ed26'),
     ],
 
     'ses' => [
