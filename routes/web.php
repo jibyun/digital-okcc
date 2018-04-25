57238b065e688cd59bbceda4025d9f40bec718d4
Gyoun Lee <gyounlee@yahoo.ca>
2018-04-25 17:06:07 -0400

    Localization for JS

--- routes/web.php
+++ routes/web.php
@@ -135,3 +135,30 @@ Route::get('okcc/memberList/settings', 'Rest\MemberList\MemberListController@get
  * 
  */
 Route::get('okcc/util/adduser/{id}/{password}', 'Rest\Util\UtilsController@addUser');
+
+/*
+|--------------------------------------------------------------------------
+| Localization for Javascript
+| This code is from 
+| https://medium.com/@serhii.matrunchyk/using-laravel-localization-with-javascript-and-vuejs-23064d0c210e
+|--------------------------------------------------------------------------
+*/
+Route::get('/js/lang.js', function () {
+    $strings = Cache::rememberForever('lang.js', function () {
+        $lang = config('app.locale');
+
+        $files   = glob(resource_path('lang/' . $lang . '/*.php'));
+        $strings = [];
+
+        foreach ($files as $file) {
+            $name           = basename($file, '.php');
+            $strings[$name] = require $file;
+        }
+
+        return $strings;
+    });
+
+    header('Content-Type: text/javascript');
+    echo('window.i18n = ' . json_encode($strings) . ';');
+    exit();
+})->name('assets.lang');
