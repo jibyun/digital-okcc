d3eda70edb0e96c145426be9b0bf6f7f4bbc9e6c
kiwoong <kw2446@gmail.com>
2018-05-14 12:04:44 -0400

    멤버 상세 정보에서 가족탭과 부서소속정보 탭을 통합하여 Basic탭으로

--- public/js/MemberList/memberDetail.js
+++ public/js/MemberList/memberDetail.js
@@ -8,19 +8,23 @@ var current_member;
 $( function() {
     $( "#tabs" ).tabs();
 
-    // select box data setting
-    var url=codesURL+ '?category_id[]=1&category_id[]=2&category_id[]=4&category_id[]=6&category_id[]=7&category_id[]=8';
-    restApiCall(url, "GET",null,catesuccess , null);
-    
-
+  
     //Get member info by bt_table clicked
     $('#bt_table').on('click-cell.bs.table', function (field, column, row, rec) {
 
         currentMemberId = Number(rec.id);
-        var url=memberUrl+"/"+currentMemberId;
-
+        GetMemberInfo(currentMemberId);
       
-        restApiCall(url, "GET", null, memberGetSuccess, null);
+        
+    });
+
+
+    //Get member info by bt_table clicked
+    $('#family_table').on('click-cell.bs.table', function (field, column, row, rec) {
+
+        currentMemberId = Number(rec.id);
+        GetMemberInfo(currentMemberId);
+      
         
     });
 
@@ -51,157 +55,79 @@ function memberGetSuccess(response) {
     current_member = JSON.parse(response.data);
     openShowPanel(current_member);   
     memberDetailSelectHandler();
+    
 }
+
 
 
 function fillFamily(familys){
-
+    var table = $('#family_table');
+    var tableColumn = [{
+        field: 'english_name',
+        width: '20%',
+        title: 'english_name',
+        color:'blue'
+    }, {
+        field: 'relation_txt',
+        width: '20%',
+        title: 'relation'
+    },{
+        field: 'id',
+        visible:false
+    }];
+    
    
-    $('#tabs-family').children().remove();
-    if(familys!=null && familys.length>0)
-    {
-        for(var i= 0; i < familys.length; i++) {
-            var item = familys[i];
-            var parentid = 'family_' + i;
-          
-            $('#tabs-family').append($('<ul/>', {
-                class: "list-unstyled",
-                id: parentid
-            }));
 
-            $('#'+parentid).append($('<li/>', {
-                text: item.english_name,
-                style: "display:inline-block;width:20%",
-                id: 'familyName_' + i
-            })).append($('<li/>', {
-                text: item.relation_txt,
-                style: "display:inline-block;width:40%",
-                id: 'relation_' + i
-            }));
-            
-        }
-    }
-    else{
-        $('#tabs-family').append($('<ul/>', {
-        })).append($('<li/>', {
-            text: 'There is no data.',
-            style: "display:inline-block;width:100%"
-        }));
-    }
-   
+    table.bootstrapTable({
+        columns: tableColumn,
+        pagination: false
+                
+    });
+
+    table.bootstrapTable('load', familys);
 }
-/*
-function fillVisit(visits){
-    $('#tabs-visit').children().remove();
-    if(visits.length>0)
-    {
-        for(var i= 0; i < visits.length; i++) {
-            var item = visits[i];
-            var parentid = 'visit_' + i;
-        
-            $('#tabs-visit').append($('<ul/>', {
-                class: "list-unstyled",
-                id: parentid
-            }));
-
-            
-            $('#'+parentid).append($('<li/>', {
-                text: item.visited_at,
-                style: "display:inline-block;width:20%",
-                id: 'visit_start_' + i
-            })).append($('<li/>', {
-                text: item.title,
-                style: "display:inline-block;width:40%",
-                id: 'visit_title_' + i
-            })).append($('<li/>', {
-                text: item.user.name,
-                style: "display:inline-block;width:20%",
-                id: 'visit_user_name_' + i
-            }));
-        }
-    }
-    else{
-        $('#tabs-visit').append($('<ul/>', {
-        })).append($('<li/>', {
-            text: 'There is no data.',
-            style: "display:inline-block;width:100%"
-        }));
-    }
-}*/
 
 function fillWork(works){
-    $('#tabs-4').children().remove();
-    if(works!=null && works.length>0)
-    {
-        for(var i= 0; i < works.length; i++) {
-            var item = works[i];
-            var parentid = 'visit_' + i;
-        
-            $('#tabs-3').append($('<ul/>', {
-                class: "list-unstyled",
-                id: parentid
-            }));
+    var table = $('#work_table');
+    var tableColumn = [{
+        field: 'id',
+        visible:false
+    },{
+        field: 'department_txt',
+        width: '40%',
+        title: 'Department',
+        color:'blue'
+    }, {
+        field: 'code_by_position_id.txt',
+        width: '40%',
+        title: 'Position'
+    },{
+        field: 'is_manager_txt',
+        width: '20%',
+        title: 'IsManager'
+    }];
+    
+   
 
-            $('#'+parentid).append($('<li/>', {
-                text: item.visited_at,
-                style: "display:inline-block;width:20%",
-                id: 'visit_start_' + i
-            })).append($('<li/>', {
-                text: item.title,
-                style: "display:inline-block;width:40%",
-                id: 'visit_title_' + i
-            })).append($('<li/>', {
-                text: item.user.name,
-                style: "display:inline-block;width:20%",
-                id: 'visit_user_name_' + i
-            }));
-        }
-    }
-    else{
-        $('#tabs-4').append($('<ul/>', {
-        })).append($('<li/>', {
-            text: 'There is no data.',
-            style: "display:inline-block;width:100%"
-        }));
-    }
+    table.bootstrapTable({
+        columns: tableColumn,
+        pagination: false
+                
+    });
+
+    table.bootstrapTable('load', works);
 }
+
 function openShowPanel(member) {
    
     fillShowPanel(member);
     fillFamily(member.familys);
     fillHistory(member.member_histories);
     fillVisit(member.visits);
-    fillWork(member.work); 
+    fillWork(member.member_department_maps); 
 }
 
-function catesuccess(response) {
-   
-    var category = JSON.parse(response.data);
-   
-        fillCombo( $('#selectStatusCombo'), category[0].codes, "select" );
-        fillCombo( $('#selectDutyCombo'), category[1].codes, "duty" );
-        fillCombo( $('#selectLevelCombo'), category[2].codes, "level" );
-        fillCombo( $('#selectCityCombo'), category[3].codes, "city" );
-        fillCombo( $('#selectProvinceCombo'), category[4].codes, "province" );
-        fillCombo( $('#selectCountryCombo'), category[5].codes, "country" );
-    
-}
 
-function fillCombo($element, codeData, kind) {
-    $element.empty();
-    var html = '<option value=""></option>';
-    $.each(codeData, function( index, codes ) {
-        html += '<option value="' + codes['id'] + '">' + ( kind === 'province' ? codes['kor_txt'] + ' (' + codes['txt'] + ')' : codes['txt'] )  + '</option>';
-    });
-    $element.prepend(html);
-    // The following options are available to pass into Chosen on instantiation.
-    $element.chosen({
-        case_sensitive_search: false,
-        search_contains: true, // Setting this option to true allows matches starting from anywhere within a word. 
-        no_results_text: "Oops, nothing found!",
-        placeholder_text_single: "Please Select One!",
-    });
-}
 
 function fillData(parentId,rec){
     var parentElement = $("#"+parentId);
@@ -212,3 +138,12 @@ function fillData(parentId,rec){
     }
 
 }
+
+function GetMemberInfo(memberId)
+{
+    var url=memberUrl+"/"+memberId;
+    restApiCall(url, "GET", null, memberGetSuccess, null);
+    
+}
+
+
